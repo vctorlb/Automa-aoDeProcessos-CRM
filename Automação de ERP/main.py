8cdffc2ffe981c27c18b3f4398e29cc8eaf8f3c2
Victor Lopes <54962900+vctorlb@users.noreply.github.com>
2025-04-21 11:22:05 -0300

    Primeiro commit do projeto Bikcraft

---
+++ Automação de ERP/main.py
@@ -0,0 +1,16 @@
+# fakturama -> link na descrição -> ERP de exemplo que vamos usar
+# pyautogui -> é a ferramenta que a gente vai usar pra automatizar ERP
+
+# Escrever a lógica que a gente vai construir em português o passo a passo
+import pyautogui
+import pyperclip
+import subprocess
+import time
+import pandas as pd
+
+# Abrir o ERP (Fakturama)
+subprocess.Popen([r"C:\Program Files\Fakturama2\Fakturama.exe"])
+
+
+# fakturama está aberto
+
